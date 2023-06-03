464fecfd85156c208da9ebbd2b2bb4c9875eda18
ragecpp <mathematicscpu@gmail.com>
2023-06-03 12:47:21 +0800

    update libgit2.sh

--- ubuntu-22-04/libgit2.sh
+++ ubuntu-22-04/libgit2.sh
@@ -15,5 +15,61 @@ clear_path() {
 
 generate_build() {
   clear_path "$build_path"
-  
+  # https://github.com/libgit2/libgit2#advanced-usage
+  cmake -DCMAKE_CXX_STANDARD=17 \
+        -DCMAKE_INSTALL_BINDIR="$install_path" \
+        -DCMAKE_INSTALL_LIBDIR="$install_path" \
+        -DCMAKE_INSTALL_INCLUDEDIR="$install_path" \
+        -DBUILD_SHARED_LIBS:BOOL=OFF \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DUSE_THREADS:BOOL=OFF \
+        -S "$source_path" \
+        -B "$build_path"
 }
+
+build() {
+  cmake --build "$build_path" -- -j$(nproc)
+}
+
+clear() {
+  rm -rf "$build_path"
+  rm -rf "$install_path"
+}
+
+test() {
+  ctest --test-dir "$build_path" -V -- -j$(nproc)
+}
+
+install() {
+  cmake --build "$build_path" --target install -- -j$(nproc)
+}
+
+main() {
+  case $1 in
+  -g)
+    generate_build
+  ;;
+  -b)
+    build
+  ;;
+  -i)
+    install
+  ;;
+  -t)
+    test
+  ;;
+  -c)
+    clear
+  ;;
+  *)
+    if [ -z "$1" ]; then
+      generate_build && build && test && install
+    else
+      echo "不支持"
+      exit 1
+    fi
+  ;;
+  esac
+}
+
+main "$@" || exit 1
