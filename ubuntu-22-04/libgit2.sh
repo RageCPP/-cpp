c53790034834fdd536e9430ad85c73951ac4da20
ragecpp <mathematicscpu@gmail.com>
2023-06-06 19:39:12 +0800

    update

--- ubuntu-22-04/libgit2.sh
+++ ubuntu-22-04/libgit2.sh
@@ -5,6 +5,12 @@ source_path="$root_path/../libgit2/"
 build_path="$root_path/libgit2_build"
 install_path="$root_path/libgit2_install"
 
+
+ZLIB_ROOT_install="$root_path/zlib_install/"
+ZLIB_LIBRARY_install="$root_path/zlib_install/lib/"
+OPENSSL_ROOT_install="$root_path/openssl_install/"
+OPENSSL_LIBRARY_install="$root_path/openssl_install/lib64/"
+
 clear_path() {
   if [ ! -d "$1" ]; then
     mkdir "$1"
@@ -12,18 +18,31 @@ clear_path() {
     rm -rf "$1/*"
   fi
 }
+# -DZLIB_LIBRARY= "$ZLIB_LIBRARY_install" \
+# -DOPENSSL_SSL_LIBRARY= "$OPENSSL_SSL_LIBRARY_install" \
+# -DZLIB_ROOT_DIR="${ZLIB_ROOT_install}" \
 
 generate_build() {
   clear_path "$build_path"
   # https://github.com/libgit2/libgit2#advanced-usage
-  cmake -DCMAKE_CXX_STANDARD=17 \
-        -DCMAKE_INSTALL_BINDIR="$install_path" \
-        -DCMAKE_INSTALL_PREFIX="$install_path" \
-        -DBUILD_SHARED_LIBS:BOOL=OFF \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DUSE_THREADS:BOOL=OFF \
-        -S "$source_path" \
-        -B "$build_path"
+  # DUSE_BUNDLED_ZLIB 必须开启否则zlib链接错误
+  # DCMAKE_FIND_ROOT_PATH 设置使用指定的ZLIB静态库
+  cmake \
+    -DUSE_BUNDLED_ZLIB=ON \
+    -DUSE_SSH:BOOL=OFF \
+    -DUSE_HTTPS:BOOL=OFF \
+    -DUSE_THREADS:BOOL=OFF \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_FIND_ROOT_PATH=${ZLIB_ROOT_install} \
+    -DOPENSSL_ROOT_DIR=${OPENSSL_ROOT_install} \
+    -DOPENSSL_LIBRARIES=${OPENSSL_LIBRARY_install} \
+    -DCMAKE_INSTALL_BINDIR="$install_path" \
+    -DCMAKE_INSTALL_PREFIX="$install_path" \
+    -DBUILD_SHARED_LIBS:BOOL=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLINK_WITH_STATIC_LIBRARIES:BOOL=ON \
+    -S "$source_path" \
+    -B "$build_path" 
 }
 
 build() {
