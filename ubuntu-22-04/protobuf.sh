c8ff1c910c566d9ba97915eae7ef058b4dc29911
ragecpp <mathematicscpu@gmail.com>
2023-08-19 15:38:32 +0800

    add share lib support

--- ubuntu-22-04/protobuf.sh
+++ ubuntu-22-04/protobuf.sh
@@ -1,47 +1,116 @@
 #!/bin/sh
 
 root_path="$(pwd)"
+clear_sh_path="$root_path/common_sh/clear.sh"
+
 source_path="$root_path/../protobuf/"
+
 build_path="$root_path/protobuf_build/"
 install_path="$root_path/protobuf_install/"
 
+static_build_path="$build_path/static/"
+static_install_path="$install_path/static/"
+
+share_build_path="$build_path/share/"
+share_install_path="$install_path/share/"
+
 google_install_path="$root_path/googletest_install/"
-abseil_cpp_install_path="${root_path}/abseil-cpp_install/"
 
+static_abseil_cpp_install_path="${root_path}/abseil-cpp_install/static/"
+share_abseil_cpp_install_path="${root_path}/abseil-cpp_install/share/"
 
-clear_path() {
-  if [ ! -d "$1" ]; then
-    mkdir "$1"
-  else
-    rm -rf "$1/*"
-  fi
-}
+static_generate_build() {
+  eval "sh ${clear_sh_path} ${static_build_path}"
 
-generate_build() {
-  clear_path "$build_path"
-  if [ -d "$google_install_path" -a -d "$abseil_cpp_install_path" ]; then
+  if [ -d "$google_install_path" -a -d "$static_abseil_cpp_install_path" ]; then
     cmake -DCMAKE_CXX_STANDARD=17 \
           -DCMAKE_BUILD_TYPE=Release \
-          -DCMAKE_PREFIX_PATH:PATH="${google_install_path};${abseil_cpp_install_path}" \
+          -DCMAKE_PREFIX_PATH:PATH="${google_install_path};${static_abseil_cpp_install_path}" \
           -Dprotobuf_ABSL_PROVIDER=ON \
           -Dprotobuf_ABSL_PROVIDER=package \
           -Dprotobuf_BUILD_TESTS=ON \
           -Dprotobuf_USE_EXTERNAL_GTEST=ON \
           -S "$source_path" \
-          -B "$build_path"
+          -B "$static_build_path"
   else
-    rm -rf "$build_path"
+    rm -rf "$static_build_path"
     echo "protobuf generate build faild"
     exit 1
   fi
 }
 
-build() {
-  cmake --build "$build_path" --target all -- -j$(nproc)
+share_generate_build() {
+  eval "sh ${clear_sh_path} ${share_build_path}"
+
+  if [ -d "$google_install_path" -a -d "$share_abseil_cpp_install_path" ]; then
+    cmake -DCMAKE_CXX_STANDARD=17 \
+          -DCMAKE_BUILD_TYPE=Release \
+          -DCMAKE_PREFIX_PATH:PATH="${google_install_path};${share_abseil_cpp_install_path}" \
+          -Dprotobuf_ABSL_PROVIDER=ON \
+          -Dprotobuf_ABSL_PROVIDER=package \
+          -Dprotobuf_BUILD_TESTS=ON \
+          -Dprotobuf_USE_EXTERNAL_GTEST=ON \
+          -Dprotobuf_BUILD_SHARED_LIBS=ON \
+          -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+          -S "$source_path" \
+          -B "$share_build_path"
+  else
+    rm -rf "$share_build_path"
+    echo "protobuf generate build faild"
+    exit 1
+  fi
 }
 
-install() {
-  cmake --install "$build_path" --prefix="$install_path"
+static_build() {
+  cmake --build "$static_build_path" --target all -- -j$(nproc)
+}
+
+share_build() {
+  cmake --build "$share_build_path" --target all -- -j$(nproc)
+}
+
+static_install() {
+  cmake --install "$static_build_path" --prefix="$static_install_path"
+}
+
+share_install() {
+  cmake --install "$share_build_path" --prefix="$share_install_path"
+}
+
+
+
+static() {
+  case $1 in
+    -g)
+      static_generate_build
+      ;;
+    -b)
+      static_build
+      ;;
+    -i)
+      static_install
+      ;;
+    *)
+      echo "不支持"
+      ;;
+  esac
+}
+
+share() {
+  case $1 in
+    -g)
+      share_generate_build
+      ;;
+    -b)
+      share_build
+      ;;
+    -i)
+      share_install
+      ;;
+    *)
+      echo "不支持"
+      ;;
+  esac
 }
 
 clear() {
@@ -51,26 +120,13 @@ clear() {
 
 main() {
   case $1 in
-  -g)
-    generate_build
-  ;;
-  -b)
-    build
-  ;;
-  -i)
-    install
-  ;;
-  -c)
-    clear
-  ;;
-  *)
-  if [ -z "$1" ]; then
-    generate_build && build && install
-  else
-    echo "不支持"
-    exit 1
-  fi
-  ;;
+    clear | static | share)
+      $1 $2
+      ;;
+    *)
+      echo "unknown command: $1"
+      exit 1;
+      ;;
   esac
 }
 
